fe752be99e40aa669e568ba3b4e85b27c292cf81
andrewglowacki <aglowacki@aiscommercial.com>
2013-11-18 21:33:22 -0500

    Added some of the main components to the game ui.

--- web-ui/src/main/webapp/js/game.js
+++ web-ui/src/main/webapp/js/game.js
@@ -24,21 +24,77 @@ Ext.onReady(function() {
 					itemId: 'playersGrid',
 					xtype: 'grid',
 					columns: [{
+						text: 'Pos.',
+						dataIndex: 'position',
+						flex: 1
+					}, {
 						text: 'Name',
-						dataIndex: 'name'
+						dataIndex: 'name',
+						flex: 3
 					}, {
 						text: 'Role',
-						dataIndex: 'role'
+						dataIndex: 'role',
+						flex: 2
 					}]
 				}]
 			}, {
 				xtype: 'panel',
 				title: 'Game',
-				flex: 6,
+				flex: 4,
 				layout: {
 					type: 'vbox',
 					align: 'stretch'
-				}
+				},
+				defaults: { margin: '8' },
+				items: [{
+					xtype: 'panel',
+					title: 'Actions',
+					flex: 1
+				}, {
+					xtype: 'panel',
+					title: 'Game Status',
+					layout: {
+						type: 'hbox',
+						align: 'middle'
+					},
+					items: [{	
+				        xtype: 'label',
+						text: 'Game status here...',
+						margin: '5 0 5 0',
+						width: '100%',
+						style: 'text-align: center'
+					}]
+				}, {
+					xtype: 'panel',
+					title: 'Game Progress',
+					flex: 1
+				}, {
+					xtype: 'panel',
+					margin: '0',
+					border: 0,
+					flex: 2,
+					defaults: { margin: '8' },
+					layout: {
+						type: 'hbox',
+						align: 'stretch'
+					},
+					items: [{
+						xtype: 'grid',
+						title: 'Proposal History',
+						flex: 1,
+						columns: [{ text: 'Index', dataIndex: 'index', width: '30' },
+						          { text: 'Leader', dataIndex: 'leader', flex: 1 }, 
+						          { text: 'Proposal', dataIndex: 'proposal', flex: 3 }, 
+						          { text: 'Accepts', dataIndex: 'accepts', width: '30' },
+						          { text: 'Declines', dataIndex: 'declines', width: '30' }]
+					}, {
+						xtype: 'grid',
+						title: 'Voting History',
+						flex: 1,
+						columns: [{ text: 'Name', dataIndex: 'name', flex: 2 },
+						          { text: 'Vote', dataIndex: 'vote', flex: 1 }]
+					}]
+				}]
 			}]
 		}]
 	});
