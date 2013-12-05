300352022d0e0e3d6e5b6ab220faa20829cfb42b
andrewglowacki <aglowacki@aiscommercial.com>
2013-12-04 20:47:55 -0500

    [Issue #9] Added proposal history loading to game ui.

--- web-ui/src/main/webapp/js/game.js
+++ web-ui/src/main/webapp/js/game.js
@@ -75,7 +75,8 @@ function loadDisplay(gameId, userName) {
 						xtype: 'button', 
 						flex: 1,
 						handler: function() {
-							Ext.Msg.alert("Clicked", "Mission " + this.mission);
+							var mission = this.mission;
+							Ext.StoreManager.lookup('proposalHistory').loadProposals(mission);
 						}
 					},
 					items: [{ mission: 0, text: 'Mission 1' }, 
@@ -101,13 +102,59 @@ function loadDisplay(gameId, userName) {
 						          { text: 'Leader', dataIndex: 'leader', flex: 1 }, 
 						          { text: 'Proposal', dataIndex: 'proposal', flex: 3 }, 
 						          { text: 'Accepts', dataIndex: 'accepts', width: '30' },
-						          { text: 'Declines', dataIndex: 'declines', width: '30' }]
+						          { text: 'Declines', dataIndex: 'declines', width: '30' }],
+						store: {
+							storeId: 'proposalHistory',
+							fields: ['index', 'leader', 'proposal', 'accepts', 'declines', 'votes'],
+							loadProposals: function(mission) {
+								var me = this;
+								Ext.Ajax.request({
+									url: '/server/proposals',
+									params: {
+										gameId: gameId,
+										round: mission
+									},
+									success: function(response) {
+										var wrapper = Ext.JSON.decode(response.responseText);
+										if (!isSuccessful("Proposal Load", wrapper)) {
+											return;
+										}
+										var proposals = wrapper.data;
+										var proposalsToLoad = [];
+										var index = 1;
+										Ext.Array.each(proposals, function(proposal) {
+											var accepts = 0;
+											var declines = 0;
+											for (var player in proposal.votes) {
+												if (proposal.votes[player] == 'SEND') {
+													accepts++;
+												} else {
+													declines++;
+												}
+											}
+											Ext.Array.push(proposalsToLoad, {
+												index: index,
+												leader: proposal.leader,
+												accepts: accepts,
+												declines: declines,
+												votes: proposal.votes
+											});
+										});
+										me.loadData(proposalsToLoad);
+									}
+								});
+							}
+						}
 					}, {
 						xtype: 'grid',
 						title: 'Voting History',
 						flex: 1,
 						columns: [{ text: 'Name', dataIndex: 'name', flex: 2 },
-						          { text: 'Vote', dataIndex: 'vote', flex: 1 }]
+						          { text: 'Vote', dataIndex: 'vote', flex: 1 }],
+						store: {
+							storeId: 'votingHistory',
+							fields: ['name', 'vote']
+						}
 					}]
 				}]
 			}]
