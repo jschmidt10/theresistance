8c073b2b50e9a47d71e91ac33d6585f6d328c791
andrewglowacki <aglowacki@aiscommercial.com>
2013-12-22 15:32:26 -0500

    Removed methods from the global scope in game.js.

--- web-ui/src/main/webapp/js/game.js
+++ web-ui/src/main/webapp/js/game.js
@@ -1,40 +1,40 @@
-
-var clientGameConfig = {};
-
-function loadDisplay(gameId, userName) {
-	
-	Ext.create('js.game.GameUI', {
-		gameId: gameId,
-		userName: userName,
-		gameConfig: clientGameConfig
-	});
-	
-	Ext.Ajax.request({
-		url: '/server/config',
-		params: { gameId: gameId },
-		success: function(response) {
-			var wrapper = Ext.JSON.decode(response.responseText);
-			if (!isSuccessful("Game Configuration Load", wrapper)) {
-				return;
-			}
-			clientGameConfig = wrapper.data;
-			setMissionButtonText();
-		}
-	});
-}
-
-function setMissionButtonText() {
-	var missionButtons = Ext.ComponentQuery.query('#missions')[0].query('button');
-	var missions = clientGameConfig.missions;
-	for (var i = 0; i < missionButtons.length; i++) {
-		missionButtons[i].setText(
-				"Mission " + (i + 1) + "<br/>" + 
-				"Players Needed: " + missions[i].numParticipants + "<br/>" +
-				"Fails Needed: " + missions[i].requiredFails);
-	}
-}
-
 Ext.onReady(function() {
+
+	var clientGameConfig = {};
+
+	function loadDisplay(gameId, userName) {
+		
+		Ext.create('js.game.GameUI', {
+			gameId: gameId,
+			userName: userName,
+			gameConfig: clientGameConfig
+		});
+		
+		Ext.Ajax.request({
+			url: '/server/config',
+			params: { gameId: gameId },
+			success: function(response) {
+				var wrapper = Ext.JSON.decode(response.responseText);
+				if (!isSuccessful("Game Configuration Load", wrapper)) {
+					return;
+				}
+				clientGameConfig = wrapper.data;
+				setMissionButtonText();
+			}
+		});
+	}
+
+	function setMissionButtonText() {
+		var missionButtons = Ext.ComponentQuery.query('#missions')[0].query('button');
+		var missions = clientGameConfig.missions;
+		for (var i = 0; i < missionButtons.length; i++) {
+			missionButtons[i].setText(
+					"Mission " + (i + 1) + "<br/>" + 
+					"Players Needed: " + missions[i].numParticipants + "<br/>" +
+					"Fails Needed: " + missions[i].requiredFails);
+		}
+	}
+	
 	var game = Ext.Object.fromQueryString(location.search.substring(1));
 	if (game.gameId && game.userName) {
 		loadDisplay(game.gameId, game.userName);
